836d8266dc148351b33982d71cd4d20e907612c1
darsvid <d.svidzinska@gmail.com>
2018-03-01 10:23:54 +0200

    simplify and update the main function

--- README.md
+++ README.md
@@ -3,16 +3,16 @@ Tools to describe spatial structures and test for the presence of spatial correl
 ## Scripts
 
 *./scripts*
-* *geostats_points_function.R* -- the function performs preliminary data analysis and calculates the parameters necessary for the assesmant of semivariance and covarance, namely sample size, lag, number of distance classes. It also creates four directional profiles and a random sample.
+* *geostats_points_function.R* -- the function performs preliminary data analysis and calculates the parameters necessary for the assesmant of semivariance and autocorrelation, namely sample size, lag, number of distance classes. It also creates four directional profiles and a random sample.
 * *variograms.R* -- using variog4 function from geoR library the script calculates 5 variograms (4 directional + 1 omnidirectional);
-* *correlograms.R* -- using eco.correlog function from EcoGenetics library the script calculates 5 correlograms (4 directional + 1 omnidirectional). Both Moran's I and Geary's C autocorrelations can be computed.
+* *correlograms.R* -- using eco.correlog function from EcoGenetics library the script calculates 5 correlograms (4 directional + 1 omnidirectional). Both Moran's I and Geary's c autocorrelations can be computed.
 
 
 ## Usage
 
 ```
 source(geostats_points_function.R)
-geostats.points.function(raster.data = "raster.tif", alpha = 0.05)
+geostats.points.function(raster.data = "raster.tif") #sample.size = 1000, dist.of.interest = NULL, lag.of.interest = NULL
 source(variograms.R)
 source(correlograms.R)
 ```
--- scripts/geostats_points_function.R
+++ scripts/geostats_points_function.R
@@ -3,19 +3,17 @@
 # 
 # This function performs preliminary data analysis and calculates the parameters
 # necessary for the assesmant of univariate structure functions, namely
-# semivariance and covarance.
+# semivariance and autocorrelation.
 #
 # Arguments:
 #       - mandatory:
 #                     - raster.data: the raster to be analyzed
-#                     - alpha: significance level alpha (e.g. 0.05)
-#                       corresponding to a certain p-value (e.g. 0.95) or
-#                       confidence level (e.g. 95%). Should take values of 0.05
-#                       (default), 0.01, or 0.001.          
+#                     - sample.size: number of pixels to be sampled. Usually
+#                       sample from 1000 (the default) to 3000 points is good
+#                       enough to produce reliable results. Too large samples
+#                       require significant computing resources and cannot be
+#                       digested by GeoR and EcoGenetics libraries       
 #       - optional:
-#                     - r.matrix: radius of circular matrix (in the raster
-#                       projection measurment units, usually meters) to calculate
-#                       Moran's I index of spatial autocorrelation
 #                     - dist.of.interest: distance of interest to limit the
 #                       maximum distance calculations (in the measurment units
 #                       of the raster projection, usually meters)
@@ -43,7 +41,7 @@
 #-------------------------------------------------------------------------------
 
 geostats.points.function <- function(raster.data = "path to the raster",
-                                     alpha = 0.05, r.matrix = NULL,
+                                     sample.size = 1000,
                                      dist.of.interest = NULL,
                                      lag.of.interest = NULL){
         
@@ -59,87 +57,7 @@ geostats.points.function <- function(raster.data = "path to the raster",
 #-------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------#
-# 1. INTRODUCE HELPING FUNCTIONS
-#-------------------------------------------------------------------------------
-        # the function to remove NA's from SpatialDataFrame Object
-        # from here http://tinyurl.com/nas-omit
-        # x - SpatialDataFrame Object
-        # margin - remove rows (1) or columns (2)
-        sp.na.omit <- function(x, margin=1) {
-                if (!inherits(x, "SpatialPointsDataFrame") & !inherits(x, "SpatialPolygonsDataFrame")) 
-                        stop("MUST BE sp SpatialPointsDataFrame OR SpatialPolygonsDataFrame CLASS OBJECT") 
-                na.index <- unique(as.data.frame(which(is.na(x@data),arr.ind=TRUE))[,margin])
-                if(margin == 1) {  
-                        cat("DELETING ROWS: ", na.index, "\n") 
-                        return( x[-na.index,]  ) 
-                }
-                if(margin == 2) {  
-                        cat("DELETING COLUMNS: ", na.index, "\n") 
-                        return( x[,-na.index]  ) 
-                }
-        }
-        
-        # function to make a circular weights matrix of given radius and resolution
-        # from here http://tinyurl.com/circ-matrix
-        # Important: radius must be an even multiple of resolution
-        make.circ.matrix<-function(radius, res){
-                circ.matrix<-matrix(0, nrow=1+(2*radius/res), ncol=1+(2*radius/res))
-                dimnames(circ.matrix)[[1]]<-seq(-radius, radius, by=res)
-                dimnames(circ.matrix)[[2]]<-seq(-radius, radius, by=res)
-                sweeper<-function(mat){
-                        for(row in 1:nrow(mat)){
-                                for(col in 1:ncol(mat)){
-                                        dist<-sqrt((as.numeric(dimnames(mat)[[1]])[row])^2 +
-                                                           (as.numeric(dimnames(mat)[[1]])[col])^2)
-                                        if(dist<=radius) {mat[row, col]<-1}
-                                }
-                        }
-                        return(mat)
-                }
-                out<-sweeper(circ.matrix)
-                center <- ((radius/res)+1)
-                out[center, center] <- 0
-                return(out)
-        }
-        
-        # sample size function
-        # so far the 3 most common alpha values are possible
-        # 0.05 (default), 0.01, and 0.001
-        sample.size = function(raster, alpha=0.05, matrix=FALSE) {
-                if (alpha==0.05){
-                        z.value <- 1.96
-                } else if (alpha==0.01){
-                        z.value <- 2.58
-                } else if (alpha==0.001){
-                        z.value <- 3.09
-                }
-                sd <- cellStats(raster, stat = 'sd', na.rm=TRUE)
-                mean <- cellStats(raster, stat = 'mean', na.rm=TRUE)
-                pop <- cellStats(!is.na(gradient.raster), 'sum')
-                if (matrix == FALSE){
-                        ss1 <- (z.value^2*sd^2)/(mean*alpha)^2
-                        ss2 <- ss1/(1+(ss1/pop))
-                        sample <- list((round(ss2, 0)))
-                        names(sample) <- 'sample size'
-                } else if (matrix == TRUE) {
-                        ss1 <- (z.value^2*sd^2)/(mean*alpha)^2
-                        ss2 <- ss1/(1+(ss1/pop))
-                        moran.cor <- Moran(raster, w = weight.matrix)
-                        moran.cor <- ifelse(moran.cor<0, 0, moran.cor)
-                        adj.ss <- ss2*((1-moran.cor)/(1+moran.cor))
-                        ss <- (round(ss2, 0))
-                        adj.ss <- round(adj.ss, 0)
-                        sample <- list(adj.ss, ss)
-                        names(sample) <- c('adjust_sample', 'full_sample') 
-                }
-                return(sample)
-                
-        }
-#-------------------------------------------------------------------------------
-
-        
-#------------------------------------------------------------------------------#
-# 2. IMPORT RASTER, BUILD PROFILES AND SAMPLE DATA ALONG THEM
+# 1. IMPORT RASTER, BUILD PROFILES AND SAMPLE DATA ALONG THEM
 #-------------------------------------------------------------------------------
         gradient.raster <- raster(raster.data)
         # obtain information on projection
@@ -190,11 +108,6 @@ geostats.points.function <- function(raster.data = "path to the raster",
                 sp <- SpatialPoints(points.list[i], proj4string = CRS(proj.string))
                 transect[[i]] <- as(sp, "SpatialLines")
                 trans.points[[i]] <- remove.duplicates(ExtractAlongTransect(transect[[i]], gradient.raster)[[1]])
-                if (anyNA(trans.points[[i]]@data[,2]) == TRUE){
-                        trans.points[[i]] <- sp.na.omit(trans.points[[i]])
-                }
-                # correct distance origin to 0
-                trans.points[[i]]@data[[1]] <- trans.points[[i]]@data[[1]] - min(trans.points[[i]]@data[[1]])
                 writeOGR(obj = trans.points[[i]], dsn ="./data",
                          layer = paste0("profile_points_", names(points.list[i])),
                          driver = "ESRI Shapefile", overwrite_layer=TRUE)
@@ -213,20 +126,9 @@ geostats.points.function <- function(raster.data = "path to the raster",
 #-------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------#
-# 3. DEFINE OPTIMAL SAMPLE SIZE AND CREATE RANDOM SAMPLE                       
+# 2. CREATE RANDOM SAMPLE                       
 #-------------------------------------------------------------------------------
-        # create matrix to measure global autocorrelation
-        matrix <- ifelse(is.null(r.matrix)==TRUE, FALSE, TRUE)
-        if (matrix==TRUE){
-                weight.distance <- r.matrix
-                weight.radius <- (as.integer(weight.distance/cell.size))*cell.size
-                weight.matrix <- make.circ.matrix(radius = weight.radius, res = cell.size)
-        }
-        
-        sample <- sample.size(gradient.raster, alpha = alpha, matrix = matrix)
-        
-        # create random sample
-        random.sample.spdf <- sampleRandom(gradient.raster, size = sample[[1]],
+        random.sample.spdf <- sampleRandom(gradient.raster, size = sample.size,
                                            cells = TRUE, xy = TRUE, sp = TRUE,
                                            na.rm = TRUE)
         writeOGR(obj = random.sample.spdf, dsn ="./data", layer = "sample_points",
@@ -234,19 +136,21 @@ geostats.points.function <- function(raster.data = "path to the raster",
 #-------------------------------------------------------------------------------
         
 #------------------------------------------------------------------------------#
-# 4. DEFINE OPTIMUM NUMBER OF CLASSES AND BINS                                 #
+# 3. DEFINE OPTIMUM NUMBER OF CLASSES AND BINS                                 #
 #-------------------------------------------------------------------------------
-        # calculate number of classes from the sample size
-        n.pairs <- (sample[[1]]*(sample[[1]]-1))/2 # the number of pairs in the distance matrix
-        n.classes <- as.integer(1 + 3.322*log10(n.pairs)) # Sturges' rule
+        # calculate maximum possible number of classes
+        valid.pixels <- cellStats(!is.na(gradient.raster), 'sum')
+        n.pairs <- (valid.pixels*(valid.pixels-1))/2 # the number of pairs in the distance matrix
+        n.classes <- round(1 + 3.322*log10(n.pairs)) # Sturges' rule
         dist.of.interest <- ifelse(is.null(dist.of.interest)==TRUE,
                                    max(profiles.df$distance)/2,
                                    dist.of.interest)
         
         if (is.null(lag.of.interest) == TRUE) {
-                bin.size <- ifelse(test = ((dist.of.interest/n.classes)%%10) <= 5,
-                                   yes = (dist.of.interest/n.classes) - ((dist.of.interest/n.classes)%%10),
-                                   no = (dist.of.interest/n.classes) + (10 - ((dist.of.interest/n.classes)%%10)))
+                bin.size <- ifelse(test = ((dist.of.interest/n.classes)%%5) <= 2.5,
+                                   yes = (dist.of.interest/n.classes) - ((dist.of.interest/n.classes)%%5),
+                                   no = (dist.of.interest/n.classes) + (5 - ((dist.of.interest/n.classes)%%5)))
+                
                 # calculate bins' breaks
                 bins.sum <- 0
                 bins.breaks <- 0
@@ -269,7 +173,6 @@ geostats.points.function <- function(raster.data = "path to the raster",
         
         # create and save data for further analysis
         sample.geodata <- as.geodata(random.sample.spdf, data.col = 4)
-        saveRDS(sample, "./data/sample.rds")
         saveRDS(sample.geodata, "./data/sample_geodata.rds")
         saveRDS(bins.centers, "./data/bins_centers.rds")
         saveRDS(bins.breaks, "./data/bins_breaks.rds")
